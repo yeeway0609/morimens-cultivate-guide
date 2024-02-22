09178fc50396e3e42fce2ef2169060c79edfd13a
yeeway <yiwei.suuu@gmail.com>
2024-02-22 12:36:22 +0800

    update AwakerFilter component style

--- src/components/AwakerFilter.tsx
+++ src/components/AwakerFilter.tsx
@@ -28,47 +28,13 @@ export default function AwakerFilter() {
   }, 300);
 
   return (
-    <section className="bg-DarkGray flex justify-between h-10 w-full items-center px-4 text-xs font-medium text-white">
-      <div
-        onClick={() => handleCareerFilter("")}
-        className={`${careerFilter == "" ? "text-Golden" : "text-white"}`}
-      >
-        全選
-      </div>
-      <div
-        onClick={() => handleCareerFilter("混沌")}
-        className={`${careerFilter == "混沌" ? "text-Golden" : "text-white"} relative`}
-      >
-        <img className="absolute -ml-2" src="/icon-career-混沌.svg" alt="career icon" />
-        <span>混沌</span>
-      </div>
-      <div
-        onClick={() => handleCareerFilter("深海")}
-        className={`${careerFilter == "深海" ? "text-Golden" : "text-white"} relative`}
-      >
-        <img className="absolute -ml-2" src="/icon-career-深海.svg" alt="career icon" />
-        <span>深海</span>
-      </div>
-      <div
-        onClick={() => handleCareerFilter("血肉")}
-        className={`${careerFilter == "血肉" ? "text-Golden" : "text-white"} relative`}
-      >
-        <img className="absolute -ml-2 -mt-[2px]" src="/icon-career-血肉.svg" alt="career icon" />
-        <span>血肉</span>
-      </div>
-      <div
-        onClick={() => handleCareerFilter("超維")}
-        className={`${careerFilter == "超維" ? "text-Golden" : "text-white"} relative`}
-      >
-        <img className="absolute -ml-2" src="/icon-career-超維.svg" alt="career icon" />
-        <span>超維</span>
-      </div>
-      <div className="relative w-32 h-5 border-[0.5px] rounded-full bg-transparent flex justify-between">
+    <section className="bg-DarkGray w-full items-center px-4 py-3 text-xs font-medium text-white">
+      <div className="relative w-full h-5 mb-2 border-[0.5px] rounded-full bg-transparent flex justify-between">
         <label htmlFor="search" className="sr-only">
           Search
         </label>
         <input
-          className="w-20 ml-2 bg-transparent text-[8px] outline-none"
+          className="w-4/5 ml-3 bg-transparent text-[8px] outline-none"
           placeholder="輸入喚醒體名稱..."
           onChange={(e) => handleSearch(e.target.value)}
           defaultValue={searchParams.get('query')?.toString()}
@@ -78,6 +44,42 @@ export default function AwakerFilter() {
             <path d="M7.66667 14.3333C11.3486 14.3333 14.3333 11.3486 14.3333 7.66667C14.3333 3.98477 11.3486 1 7.66667 1C3.98477 1 1 3.98477 1 7.66667C1 11.3486 3.98477 14.3333 7.66667 14.3333Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
             <path d="M16.0005 15.9998L12.3755 12.3748" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
           </svg>
+        </div>
+      </div>
+      <div className="flex justify-around">
+        <div
+          onClick={() => handleCareerFilter("")}
+          className={`${careerFilter == "" ? "text-Golden" : "text-white"}`}
+        >
+          全選
+        </div>
+        <div
+          onClick={() => handleCareerFilter("混沌")}
+          className={`${careerFilter == "混沌" ? "text-Golden" : "text-white"} relative`}
+        >
+          <img className="absolute -ml-2" src="/icon-career-混沌.svg" alt="career icon" />
+          <span>混沌</span>
+        </div>
+        <div
+          onClick={() => handleCareerFilter("深海")}
+          className={`${careerFilter == "深海" ? "text-Golden" : "text-white"} relative`}
+        >
+          <img className="absolute -ml-2" src="/icon-career-深海.svg" alt="career icon" />
+          <span>深海</span>
+        </div>
+        <div
+          onClick={() => handleCareerFilter("血肉")}
+          className={`${careerFilter == "血肉" ? "text-Golden" : "text-white"} relative`}
+        >
+          <img className="absolute -ml-2 -mt-[2px]" src="/icon-career-血肉.svg" alt="career icon" />
+          <span>血肉</span>
+        </div>
+        <div
+          onClick={() => handleCareerFilter("超維")}
+          className={`${careerFilter == "超維" ? "text-Golden" : "text-white"} relative`}
+        >
+          <img className="absolute -ml-2" src="/icon-career-超維.svg" alt="career icon" />
+          <span>超維</span>
         </div>
       </div>
     </section>
